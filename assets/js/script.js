e4acf68664f281ce375ee59e5c8528e6131932f6
frapplecat <nigel1138@hotmail.com>
2022-10-22 17:40:14 +0000

    Add event listeners for DOM loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// WAIT FOR DOM TO FINISH LOADING
+// GET THE BUTTON ELEMENTS AND ADD EVENT LISTENERS TO THEM
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute('data-type') === 'submit') {
+                alert('You clicked Submit!');
+            } else {
+                let gameType = this.getAttribute('data-type');
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
